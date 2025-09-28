57ed9c171801ccd0b69bd3c1570c0425080d6566
Ashikhmina Liza <lizunchi@bk.ru>
2025-09-28 21:51:26 +0300

    Добавить в программу вывод произвольной математической операции для введенных чисел и совершить commit.

--- simple_program.py
+++ simple_program.py
@@ -4,3 +4,7 @@ print("Hello, World!")
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 print(f"Вы ввели: {num1} и {num2}")
+
+# Математическая операция - сложение
+result = num1 + num2
+print(f"Результат сложения: {num1} + {num2} = {result}")
